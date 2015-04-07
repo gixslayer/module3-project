2eaede4d5345e35a690f10b430df733ec134ed16
gixslayer <ciske@ciske-laptop>
2015-04-07 09:56:34 +0200

    Random test code.

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -1,5 +1,32 @@
 package project;
 
-public class Main {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 
+public class Main {
+	static final int PORT = 6969;
+	static final int DEVICE = 1;
+	
+	public static void main(String[] args) throws IOException {
+		InetAddress group = InetAddress.getByName(String.format("192.168.5.%d", DEVICE));
+		MulticastSocket socket = new MulticastSocket(PORT);
+		String data = "Test";
+		byte[] buf = new byte[1024];
+		
+		socket.joinGroup(group);		
+		
+		DatagramPacket send = new DatagramPacket(data.getBytes(), data.length(), group, PORT);
+		socket.send(send);
+		
+		DatagramPacket recv = new DatagramPacket(buf, buf.length);
+		socket.receive(recv);
+		String s = new String(recv.getData(), 0, recv.getLength());
+		
+		socket.leaveGroup(group);
+		socket.close();
+		
+		System.out.println("Received some data: " + s);
+	}
 }
