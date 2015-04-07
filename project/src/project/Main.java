6087da0c7aee0fed564826eab6b698c551efafb2
Kevkev1 <kevin1226@live.nl>
2015-04-07 09:26:42 +0000

    Added things

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -10,6 +10,41 @@ public class Main {
 	static final int PORT = 6969;
 	static final String GROUP = "228.9.10.11";
 	
+	public Main(String name) throws IOException {
+		InetAddress group = InetAddress.getByName(GROUP);
+		MulticastSocket socket = new MulticastSocket(PORT);
+		socket.joinGroup(group);
+		while (true) {
+			sendBroadcast(name, socket, group);
+			
+			DatagramPacket recv = receivePacket(socket, group);
+			String s = new String(recv.getData(), 0, recv.getLength());
+			System.out.println("Received some data: " + s);
+			
+			try {
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e) { }
+		}
+	}
+	
+	public DatagramPacket receivePacket(MulticastSocket socket, InetAddress group) throws IOException {
+		byte[] buf = new byte[1024];
+		DatagramPacket recv = new DatagramPacket(buf, buf.length);
+		socket.receive(recv);
+		return recv;
+	}
+	
+	public void sendBroadcast(String name, MulticastSocket socket, InetAddress group) throws IOException {
+		DatagramPacket packet = createPacket("[BC] " + name, group);
+		socket.send(packet);
+		System.out.println("Send a packet!");
+	}
+	
+	public DatagramPacket createPacket(String data, InetAddress group) {
+		return new DatagramPacket(data.getBytes(), data.length(), group, PORT);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		InetAddress group = InetAddress.getByName(GROUP);
 		MulticastSocket socket = new MulticastSocket(PORT);
@@ -28,6 +63,6 @@ public class Main {
 		socket.leaveGroup(group);
 		socket.close();
 		
-		System.out.println("Received some data: " + s);
+		//System.out.println("Received some data: " + s);
 	}
 }
