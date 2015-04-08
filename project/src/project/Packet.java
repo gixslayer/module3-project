ff8fee707aa8a31ae3f2a2b23a2dde3fc55a7f39
EdwinFl <e.t.flapper@student.utwente.nl>
2015-04-08 11:28:03 +0000

    packet

--- project/src/project/Packet.java
+++ project/src/project/Packet.java
@@ -32,9 +32,11 @@ public class Packet {
 	private void fillPacket() {
 		this.length = data.length;
 		int sourceDest = (source<<4)+destination;
+		System.out.println(source<<4);
 		int temp = data.length;
 		int typeLength = (type<<4)+(temp>>>8);
 		int length2 = temp - ((temp>>>8)<<8);
+		System.out.println(temp + " " + length2 + " " + typeLength);
 		int seq1 = seq>>>8;
 		int seq2 = seq-((seq>>>8)<<8);
 		int ack1 = ack>>>8;
@@ -43,9 +45,9 @@ public class Packet {
 		int windowSize1 = windowSize>>>8;
 		int windowSize2 = windowSize-((windowSize>>>8)<<8);
 		temp = calculateChecksum();
+		checksum = temp;
 		int checkSum1 = temp>>>8;
 		int checkSum2 = temp-((temp>>>8)<<8);
-		
 		byte[] header = new byte[]{(byte)sourceDest, (byte)typeLength, (byte)length2, (byte)seq1, (byte)seq2, (byte)ack1, (byte)ack2, (byte)flags, (byte)windowSize1, (byte)windowSize2, (byte)checkSum1, (byte)checkSum2};
 		packet = new byte[header.length+data.length];
 		System.arraycopy(header, 0, packet, 0, header.length);
@@ -70,38 +72,27 @@ public class Packet {
 	}
 	
 	private int calculateChecksum() {
-		return 0;
+		return 32000;
 	}
 	
-	public static void main(String[] args){
-		Packet p = new Packet(new byte[11]);
-		p.fillPacket();
-	}
-	
-	private long getUnsignedInt(int x) {
-	    return x & 0xFFFFFFFFL;
+	private int getUnsignedInt(int x) {
+	    return x<0?256+x:x;
 	}
 	
 	private void fillVariables() {
-		source = packet[0]>>>4;
-		destination = packet[0] - ((packet[0]>>>4)<<4);
-		type = packet[1]>>>4;
-		int part1 = (packet[1] - ((packet[1]>>>4)<<4));
-		part1 = part1<<8;
-		part1 += packet[2]>>>7 == -1? 128: 0;
-		packet[2] = (byte)(packet[2]>>>7 == -1? ((packet[2]-128)): packet[2]);
-		length = (part1) + packet[2];
-		System.out.println((byte)129);
-		
-		long aseq = ((getUnsignedInt(packet[3]))<<8) + getUnsignedInt(packet[4]); 
-		System.out.println((getUnsignedInt((int)packet[3])));
-		ack = (((int)packet[5])<<8) + packet[6]; 
+		source = getUnsignedInt(packet[0])>>>4;
+		destination = getUnsignedInt(packet[0]) - ((getUnsignedInt(packet[0])>>>4)<<4);
+		type = getUnsignedInt(packet[1])>>>4;
+		int part1 = getUnsignedInt(packet[1]) - ((getUnsignedInt(packet[1])>>>4)<<4);
+		length = (part1<<8) + getUnsignedInt(packet[2]);
+		seq = ((getUnsignedInt(packet[3]))<<8) + getUnsignedInt(packet[4]); 
+		ack = ((getUnsignedInt(packet[5]))<<8) + getUnsignedInt(packet[6]);  
 		synFlag = packet[7]>>>2 == 1;
 		ackFlag = (packet[7]-(packet[7]>>>2<<2)>>>1) == 1;
 		finFlag = (packet[7]-packet[7]>>>1<<1) == 1;
-		windowSize = (((int)packet[8])<<8) + packet[9];
-		checksum = (((int)packet[10])<<8) + packet[10];
+		windowSize = ((getUnsignedInt(packet[8]))<<8) + getUnsignedInt(packet[9]); 
+		checksum = (((int)packet[10])<<8) + packet[11];
 		
-		data = Arrays.copyOfRange(packet, 10, packet.length);
+		data = Arrays.copyOfRange(packet, 11, packet.length);
 	}
 }
