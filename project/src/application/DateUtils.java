1b64dfbb6f297464d994c086badea419d61f8f35
gixslayer <ciske@ciske-laptop>
2015-04-08 14:40:35 +0200

    Fixed epoch time, I think

--- project/src/application/DateUtils.java
+++ project/src/application/DateUtils.java
@@ -1,19 +1,9 @@
 package application;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 public final class DateUtils {
-	static Calendar calendar;
-
-	static {
-		calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
-		calendar.clear();
-		calendar.set(2011, Calendar.OCTOBER, 1);
-	}
-	
 	public static String timestampToDateString(long timestamp, String format) {
 		Date date = new Date(timestamp);
 		SimpleDateFormat formatter = new SimpleDateFormat(format);
@@ -22,6 +12,6 @@ public final class DateUtils {
 	}
 	
 	public static long getEpochTime() {
-		return calendar.getTimeInMillis();
+		return (new Date()).getTime();
 	}
 }
--- project/src/client/ClientCache.java
+++ project/src/client/ClientCache.java
@@ -105,11 +105,15 @@ public final class ClientCache {
 		List<Client> timedOutClients = new ArrayList<Client>();
 		Map<Client, Client> lostRouteClients = new HashMap<Client, Client>();
 		
+		System.out.println("[Checking timeouts]");
+		
 		synchronized(syncRoot) {
 			//long now = System.currentTimeMillis();
 			long now = DateUtils.getEpochTime();
 			
 			for(Client client : cache.values()) {
+				System.out.printf("Client %s last seen: %d diff: %d%n", client, client.getLastSeen(), now - client.getLastSeen());
+				
 				if(now - client.getLastSeen() >= TIMEOUT_DURATION) {
 					System.out.printf("Detected timeout: %s, %d%n", client, now - client.getLastSeen());
 					timedOutClients.add(client);
