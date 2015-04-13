680dab76b1ae968532f701a909442ec38af2356d
gixslayer <ciske_harsema_2@hotmail.com>
2015-04-13 03:33:49 +0200

    TcpInterface now acts as a simple pass-through to the UnicastInterface so we can test the system without a TCP layer (as we don't have a proper working one yet).

--- project/src/network/TcpInterface.java
+++ project/src/network/TcpInterface.java
@@ -18,11 +18,13 @@ public final class TcpInterface {
 	}
 	
 	public void send(InetAddress destination, Packet packet) {
-		
+		// Act as a simple pass-through to the unicast interface as we have no working TCP implementation.
+		unicastInterface.send(destination, packet);
 	}
 	
 	public void onPacketReceived(Packet packet) {
-		
+		// Act as a simple pass-through to the unicast interface as we have no working TCP implementation.
+		callbacks.onTcpPacketReceived(packet);
 	}
 	
 	public void forceClose(InetAddress remoteAddress) {
