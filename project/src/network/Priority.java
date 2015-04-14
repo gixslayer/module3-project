8256510679f69771a88cac9fb62339facfd5e718
gixslayer <ciske@ciske-laptop>
2015-04-14 11:35:13 +0200

    Priority queue implementation and first test class

---
+++ project/src/network/Priority.java
@@ -0,0 +1,19 @@
+package network;
+
+public enum Priority {
+	Low(0),
+	Normal(1),
+	High(2);
+	
+	public static final int NUM_PRIORITIES = 3;
+	
+	private final int value;
+	
+	Priority(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
---
+++ project/src/network/PriorityQueue.java
@@ -0,0 +1,45 @@
+package network;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public final class PriorityQueue<T> {
+	private Queue<T>[] queues;
+	
+	@SuppressWarnings("unchecked")
+	public PriorityQueue() {
+		this.queues = new Queue[Priority.NUM_PRIORITIES];
+		
+		for(int i = 0; i < queues.length; i++) {
+			queues[i] = new LinkedList<T>();
+		}
+	}
+	
+	public void add(T e, Priority priority) {
+		queues[priority.getValue()].add(e);
+	}
+	
+	public T poll() {
+		for(int i = Priority.NUM_PRIORITIES - 1; i > 0; i--) {
+			T value = queues[i].poll();
+			
+			if(value != null) {
+				return value;
+			}
+		}
+		
+		return null;
+	}
+	
+	public T peek() {
+		for(int i = Priority.NUM_PRIORITIES - 1; i > 0; i--) {
+			T value = queues[i].peek();
+			
+			if(value != null) {
+				return value;
+			}
+		}
+		
+		return null;
+	}
+}
